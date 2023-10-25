3f6517799dfb558a69e89dbe1c5a208dd751389b
Emmanuel (Manu) Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-10-25 17:10:52 +0300

    Create hello_world.js

---
+++ 2023-3/hello_world.js
@@ -0,0 +1,8 @@
+// the hello world program using the console.log() method
+console.log('Hello World');
+
+// the hello world program using the alert() method
+alert("Hello, World!");
+
+// the hello world program using the document.write() method
+document.write('Hello, World!');
